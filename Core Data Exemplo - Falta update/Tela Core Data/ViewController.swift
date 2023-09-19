7b143da92de076ddbf398cd1f11a9b2eb2def950
Vitor Magalhães Vieira <71799009+vamvieira@users.noreply.github.com>
2023-09-19 20:51:22 -0300

    UPDATE check
    add update check

---
+++ Core Data Exemplo - Falta update/Tela Core Data/ViewController.swift
@@ -0,0 +1,118 @@
+//
+//  ViewController.swift
+//  Tela Core Data
+//
+//  Created by Usuário Convidado on 05/09/23.
+//
+
+import UIKit
+import CoreData
+
+class ViewController: UIViewController, UITextFieldDelegate {
+    
+    var pessoaVindoDaTable: NSManagedObject?=nil
+    
+    @IBOutlet weak var txtNome: UITextField!
+    @IBOutlet weak var txtTelefone: UITextField!
+    @IBOutlet weak var txtEmail: UITextField!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        txtNome.delegate = self
+        txtTelefone.delegate = self
+        txtEmail.delegate = self
+        if pessoaVindoDaTable != nil{
+            txtNome.text = pessoaVindoDaTable?.value(forKey: "nome") as? String
+            txtTelefone.text = pessoaVindoDaTable?.value(forKey: "telefone") as? String
+            txtEmail.text = pessoaVindoDaTable?.value(forKey: "email") as? String
+        }
+    }
+    
+    func save(nome:String, tele:String, emai:String){
+        guard let appDelegate = UIApplication.shared.delegate as? AppDelegate else { return }
+        let managedContext = appDelegate.persistentContainer.viewContext
+        let entidade = NSEntityDescription.entity(forEntityName: "Pessoa", in: managedContext)!
+        
+        if pessoaVindoDaTable == nil{
+            let pessoa = NSManagedObject(entity: entidade, insertInto: managedContext)
+            pessoa.setValue(nome, forKeyPath: "nome")
+            pessoa.setValue(tele, forKeyPath: "telefone")
+            pessoa.setValue(emai, forKeyPath: "email")
+        }else{
+            let objectUpdate = pessoaVindoDaTable
+            objectUpdate?.setValue(nome, forKey: "nome")
+            objectUpdate?.setValue(tele, forKey: "telefone")
+            objectUpdate?.setValue(emai, forKey: "email")
+            do{
+                try managedContext.save()
+            }catch let error as NSError{
+                print("Não foi possível salvar \(error) ,\(error.localizedDescription)")
+            }
+        }
+        
+        func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+            if textField == txtNome{
+                txtTelefone.becomeFirstResponder()
+                return true
+            }else if textField == txtTelefone{
+                txtEmail.becomeFirstResponder()
+                return true
+            }else if textField == txtEmail{
+                salvar(textField)
+                return true
+            }else{
+                return false
+            }
+        }
+        
+        @IBAction func salvar(_ sender: Any) {
+            txtEmail.resignFirstResponder()
+            txtNome.resignFirstResponder()
+            txtTelefone.resignFirstResponder()
+            if txtNome.text!.isEmpty || txtEmail.text!.isEmpty || txtTelefone.text!.isEmpty{
+                let alerta = UIAlertController(
+                    title: "Erro",
+                    message: "Preencha todos os campos",
+                    preferredStyle: UIAlertController.Style.alert)
+                
+                alerta.addAction(UIAlertAction(
+                    title: "Ok",
+                    style: UIAlertAction.Style.cancel))
+                
+                present(alerta, animated: true)
+                return
+            }
+            
+            self.save(nome: txtNome.text!, tele: txtTelefone.text!, emai: txtEmail.text!)
+            self.navigationController?.popViewController(animated: true)
+            
+            /*
+             var msg:String
+             msg = "Ok, dados gravados para "
+             msg += txtNome.text! + " "
+             msg += txtTelefone.text!
+             
+             let alerta2 = UIAlertController(
+             title: "Aviso",
+             message: msg,
+             preferredStyle: UIAlertController.Style.alert)
+             
+             alerta2.addAction(UIAlertAction(
+             title: "Ok",
+             style: UIAlertAction.Style.cancel))
+             
+             present(alerta2, animated: true)
+             */
+            
+            
+        }
+        
+        override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+            txtEmail.resignFirstResponder()
+            txtNome.resignFirstResponder()
+            txtTelefone.resignFirstResponder()
+        }
+        
+    }
+    
+}
